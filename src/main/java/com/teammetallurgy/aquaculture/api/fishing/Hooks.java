d930b6e1dd4aa347db5ff56fcf36b3901a3c1c41
sscards55 <free-4-mel@hotmail.com>
2020-11-28 21:01:45 +0900

    Add Note Bobber sound

--- src/main/java/com/teammetallurgy/aquaculture/api/fishing/Hooks.java
+++ src/main/java/com/teammetallurgy/aquaculture/api/fishing/Hooks.java
@@ -2,6 +2,7 @@ package com.teammetallurgy.aquaculture.api.fishing;
 
 import com.teammetallurgy.aquaculture.Aquaculture;
 import net.minecraft.tags.FluidTags;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.TextFormatting;
@@ -9,6 +10,7 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Aquaculture.MOD_ID) //To get the static registering working
 public class Hooks {
+    public static final SoundEvent BOBBER_NOTE = (SoundEvent)(new SoundEvent(new ResourceLocation("aquaculture", "bobber_note_catch"))).setRegistryName(new ResourceLocation("aquaculture", "bobber_note_catch"));
     public static final Hook EMPTY = new Hook.HookBuilder().build();
     //Water
     public static final Hook IRON = new Hook.HookBuilder("iron").setDurabilityChance(0.20).setColor(TextFormatting.GRAY).build();
@@ -18,7 +20,7 @@ public class Hooks {
     public static final Hook HEAVY = new Hook.HookBuilder("heavy").setColor(TextFormatting.BOLD).setWeight(new Vector3d(0.6D, 0.15D, 0.6D)).build();
     public static final Hook DOUBLE = new Hook.HookBuilder("double").setColor(TextFormatting.DARK_GRAY).setDoubleCatchChance(0.10).build();
     public static final Hook REDSTONE = new Hook.HookBuilder("redstone").setColor(TextFormatting.RED).setCatchableWindow(35, 70).build();
-    public static final Hook NOTE = new Hook.HookBuilder("note").setColor(TextFormatting.DARK_RED).setCatchSound(SoundEvents.ENTITY_PLAYER_LEVELUP).build();
+    public static final Hook NOTE = new Hook.HookBuilder("note").setColor(TextFormatting.DARK_RED).setCatchSound(BOBBER_NOTE).build();
     public static final Hook NETHER_STAR = new Hook.HookBuilder("nether_star").setColor(TextFormatting.BLACK)/*.setFluid(FluidTags.LAVA)*/.setFluid(FluidTags.WATER).setDurabilityChance(0.50).setLuckModifier(1).build();
     //Lava
     //public static final Hook OBSIDIAN = new Hook.HookBuilder("obsidian").setColor(TextFormatting.DARK_PURPLE).setFluid(FluidTags.LAVA).build();
